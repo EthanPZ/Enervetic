2b0d5c5af52905ed1b0e5a61a59c04914a2f1b53
EthanPZ <63143619+EthanPZ@users.noreply.github.com>
2022-07-03 16:16:29 +0300

    updated & fixed

---
+++ menu.js
@@ -0,0 +1,11 @@
+function openside() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("menuclose").style.display = "block"; 
+    document.getElementById("menuopen").style.display = "none";
+}
+
+function closeside() {
+    document.getElementById("menu").style.display = "none"; 
+    document.getElementById("menuopen").style.display = "block";
+    document.getElementById("menuclose").style.display = "none";
+}
