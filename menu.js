ed335fd59c5e1c4f71017fb1188276016260a6a3
EthanPZ <63143619+EthanPZ@users.noreply.github.com>
2022-07-03 16:36:36 +0300

    fixed bugs

--- menu.js
+++ menu.js
@@ -1,11 +1,13 @@
 function openside() {
-    document.getElementById("menu").style.display = "block";
-    document.getElementById("menuclose").style.display = "block"; 
-    document.getElementById("menuopen").style.display = "none";
-}
+    if (document.getElementById("menuclose").style.display = "block") {
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("menuopen").style.display = "none";
+    }
+};
 
 function closeside() {
-    document.getElementById("menu").style.display = "none"; 
-    document.getElementById("menuopen").style.display = "block";
-    document.getElementById("menuclose").style.display = "none";
-}
+   if (document.getElementById("menuopen").style.display = "block") {
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("menuclose").style.display = "none"; 
+   } 
+};
